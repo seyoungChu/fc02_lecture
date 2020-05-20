dcd6152aadac7d4f5ba8cd7ed51777a633d1678a
seyoung800327 <seyoung@aceproject.co.kr>
2020-05-20 22:28:31 +0900

    [4번째- 게임시스템 제작]

---
+++ battleground/Assets/1.Scripts/GameData/BaseData.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// data의 기본 클래스입니다.
+/// 공통적인 데이터를 가지고 있게 되는데 , 이름만 현재 갖고 있다.
+/// 데이터의 갯수와 이름의 목록 리스트를 얻을 수 있다.
+/// </summary>
+public class BaseData : ScriptableObject
+{
+    public const string dataDirectory = "/9.ResourcesData/Resources/Data/";
+    public string[] names = null;
+
+    public BaseData() { }
+
+    public int GetDataCount()
+    {
+        int retValue = 0;
+
+        if (this.names != null)
+        {
+            retValue = this.name.Length;
+        }
+
+        return retValue;
+    }
+
+    /// <summary>
+    /// 툴에 출력하기 위한 이름 목록을 만들어주는 함수.
+    /// </summary>    
+    public string[] GetNameList(bool showID, string filterWord = "")
+    {
+        string[] retList = new string[0];
+        if(this.names == null)
+        {
+            return retList;
+        }
+
+        retList = new string[this.names.Length];
+        
+        for(int i = 0; i < this.names.Length;i++)
+        {
+            if(filterWord != "")
+            {
+                if(names[i].ToLower().Contains(filterWord.ToLower()) == false)
+                {
+                    continue;
+                }
+            }
+            if(showID)
+            {
+                retList[i] = i.ToString() + " : " + this.names[i];
+            }
+            else
+            {
+                retList[i] = this.names[i];
+            }
+        }
+
+        return retList;
+    }
+
+    public virtual int AddData(string newName)
+    {
+        return GetDataCount();
+    }
+    public virtual void RemoveData(int index)
+    {
+    }
+    public virtual void Copy(int index)
+    {
+
+    }
+
+    
+}
---
+++ battleground/Assets/1.Scripts/Manager/ResourceManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Versioning;
+using UnityEngine;
+using UnityObject = UnityEngine.Object;
+/// <summary>
+/// Resources.Load를 래핑하는 클래스.
+/// 나중엔 어셋번들로 변경됨.
+/// </summary>
+public class ResourceManager
+{
+    public static UnityObject Load(string path)
+    {
+        //지금은 리소스 로드지만 추후엔 어셋 로드로 변경됨.
+        return Resources.Load(path);
+    }
+
+    public static GameObject LoadAndInstantiate(string path)
+    {
+        UnityObject source = Load(path);
+        if(source == null)
+        {
+            return null;
+        }
+        return GameObject.Instantiate(source) as GameObject;
+    }
+}
