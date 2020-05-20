23f24ae6eed273c891dc61c096820cd02f1d8383
seyoung800327 <seyoung@aceproject.co.kr>
2020-05-20 23:02:23 +0900

    [5번째 게임 시스템]

---
+++ battleground/Assets/1.Scripts/GameData/EffectClip.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 이펙트 프리팹과 경로와 타입등의 속성 데이터를 가지고 있게 되며
+/// 프리팹 사전로딩 기능을 갖고 있고 - 풀링을 위한 기능이기도 합니다.
+/// 이펙트 인스턴스 기능도 갖고 있으며 - 풀링과 연계해서 사용하기도 합니다.
+/// </summary>
+public class EffectClip
+{   //추후 속성은 같지만 다른 이펙트 클립이 있을수 있어서 분별용.
+    public int realId = 0;
+
+    public EffectType effectType = EffectType.NORMAL;
+    public GameObject effectPrefab = null;
+    public string effectName = string.Empty;
+    public string effectPath = string.Empty;
+    public string effectFullPath = string.Empty;
+    public EffectClip() { }
+
+    public void PreLoad()
+    {
+        this.effectFullPath = effectPath + effectName;
+        if(this.effectFullPath != string.Empty && this.effectPrefab == null)
+        {
+            this.effectPrefab = ResourceManager.Load(effectFullPath) as GameObject;
+        }
+    }
+    public void ReleaseEffect()
+    {
+        if(this.effectPrefab != null)
+        {
+            this.effectPrefab = null;
+        }
+    }
+
+    /// <summary>
+    /// 원하는 위치에 내가 원하는 이펙트를 인스턴스합니다.
+    /// </summary>    
+    public GameObject Instantiate(Vector3 Pos)
+    {
+        if(this.effectPrefab == null)
+        {
+            this.PreLoad();
+        }
+        if(this.effectPrefab != null)
+        {
+            GameObject effect = GameObject.Instantiate(effectPrefab, Pos, Quaternion.identity);
+            return effect;
+        }
+        return null;
+    }
+
+
+
+}
---
+++ battleground/Assets/1.Scripts/GameData/EffectData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Xml;
+using System.IO;
+
+/// <summary>
+/// 이펙트 클립 리스트와 이펙트 파일 이름과 경로를 가지고 있으며 파일을 읽고 쓰는
+/// 기능을 가지고 있다.
+/// </summary>
+public class EffectData : BaseData
+{
+    public EffectClip[] effectClips = new EffectClip[0];
+
+    public string clipPath = "Effects/";
+    private string xmlFilePath = "";
+    private string xmlFileName = "effectData.xml";
+    private string dataPath = "Data/effectData";
+    //XML 구분자.
+    private const string EFFECT = "effect"; //저장 키.
+    private const string CLIP = "clip"; //저장 키.
+
+    private EffectData() { }
+
+}
