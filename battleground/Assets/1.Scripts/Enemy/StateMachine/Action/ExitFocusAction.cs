68733c580a5d96d3a9578fd5fc6abf590f810d75
seyoung800327 <seyoung@aceproject.co.kr>
2020-06-18 20:14:48 +0900

    [0618] state 01

---
+++ battleground/Assets/1.Scripts/Enemy/StateMachine/Action/ExitFocusAction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "PluggableAI/Actions/Exit Focus")]
+public class ExitFocusAction : Action
+{
+    public override void OnReadyAction(StateController controller)
+    {
+        controller.focusSight = false;
+        controller.variables.feelAlert = false;
+        controller.variables.hearAlert = false;
+        controller.Strafing = false;
+        controller.nav.destination = controller.personalTarget;
+        controller.nav.speed = 0f;
+    }
+    public override void Act(StateController controller)
+    {
+        
+    }
+}
